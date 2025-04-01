dc7981428028b8ac45bee932eca1b9217b711476
PaoloBe12 <112547894+PaoloBe12@users.noreply.github.com>
2025-04-01 22:59:06 +0200

    PB -> Aggiunto operazioni di analisi del testo

--- app/Http/Controllers/NewsAnalysisController.php
+++ app/Http/Controllers/NewsAnalysisController.php
@@ -4,45 +4,65 @@ namespace App\Http\Controllers;
 
 use App\Helpers\ApiResponse;
 use Illuminate\Http\Request;
-use TextAnalysis\Filters\StopWords;
-use TextAnalysis\Tokenizers\GeneralTokenizer;
 use App\Models\KeywordsPool;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Http\Client\Response;
-use Illuminate\Http\Client\RequestException;
+
+use TextAnalysis\Tokenizers\GeneralTokenizer;
+use TextAnalysis\Analysis\FreqDist;
+use TextAnalysis\Documents\TokensDocument;
+
+use TextAnalysis\Filters\StopWordsFilter;
+use TextAnalysis\Filters\LowerCaseFilter;
+use TextAnalysis\Filters\CharFilter;
+use TextAnalysis\Filters\PunctuationFilter;
+use TextAnalysis\Filters\QuotesFilter;
+
 use Exception;
 
 class NewsAnalysisController extends Controller
 {
     private $gnewsApiKey;
+    private $APITubeKey;
     private $serpApiKey;
     private $client;
 
     public function __construct()
     {
         $this->gnewsApiKey = env('GNEWS_API_KEY');
+        $this->APITubeKey = env('API_TUBE_KEY');
         $this->serpApiKey = env('SERP_API_KEY');
     }
 
-    //<1. FETCH NEWS
+    //1 -> FETCH NEWS
     public function fetchNews()
     {
         try {
-            $lastRun = Cache::get('last_news_fetch', now()->subHours(8)->format('Y-m-d\TH:i:s\Z'));
-            $currentRun = now()->format('Y-m-d\TH:i:s\Z');
+            $lastRun = Cache::get('last_news_fetch', now()->subHours(48)->format('Y-m-d\TH:i:s\Z'));
+            $currentRun = now()->format('Y-m-d');
 
-            $response = Http::withOptions(['verify' => false])->retry(1, 3)->connectTimeout(10)->accept('application/json')->get('https://gnews.io/api/v4/top-headlines?',[
-                'category' => 'technology',
-                'lang' => 'it',
-                'from' => $lastRun,
-                'to' => $currentRun,
-                'max' => 200,
-                'apikey' => $this->gnewsApiKey
+            // $response = Http::withOptions(['verify' => false])->retry(1, 3)->connectTimeout(10)->accept('application/json')->get('https://gnews.io/api/v4/top-headlines?',[
+            //     'category' => 'technology',
+            //     'lang' => 'it',
+            //     'country' => 'it',
+            //     'from' => $lastRun,
+            //     'to' => $currentRun,
+            //     'max' => 50,
+            //     'apikey' => $this->gnewsApiKey
+            // ]);
+
+            $response = Http::withOptions(['verify' => false])->retry(1, 3)->connectTimeout(10)->accept('application/json')->get('https://api.apitube.io/v1/news/top-headlines',[
+                'category.id' => 'IAB19', // Tecnologia
+                'language' => 'it', // Solo articoli in italiano
+                'source.country.code' => 'it', // Notizie da fonti italiane
+                'is_duplicate' => 'true', // Evita duplicati
+                'sort_by' => 'published_at', // Ordina per data di pubblicazione
+                'sort_order' => 'desc',
+                'api_key' => $this->APITubeKey
             ]);
 
-            Cache::put('last_news_fetch', $currentRun, 86400);
+            // Cache::put('last_news_fetch', $currentRun, 86400);
 
             return ApiResponse::success($response->json(), "News recuperate con successo", 200);
         } catch (Exception $e) {
@@ -51,33 +71,40 @@ class NewsAnalysisController extends Controller
         }
     }
 
-    // 📝 2. ANALISI TESTO
-    // public function analyzeNews()
-    // {
-    //     try {
-    //         $newsData = $this->fetchNews()->original;
-    //         $allText = '';
+    // 2 -> ANALISI TESTO
+    public function analyzeNews()
+    {
+        try {
+            $stopWordsDir = base_path('vendor/yooper/stop-words/data');
+            $stopWords = array_map('trim', file($stopWordsDir. '/stop-words_italian_2_it.txt'));
 
-    //         foreach ($newsData['articles'] as $article) {
-    //             $allText .= ' ' . $article['title'] . ' ' . $article['description'];
-    //         }
+            $newsData = $this->fetchNews()->original;
+            $articles = $newsData['data']['articles'] ?? [];
 
-    //         // Tokenizzazione + Stop Words
+            $allText = implode(' ', array_map(fn($article) => $article['title'] . ' ' . $article['description'], $articles));
+        
+            $tokenizer = new GeneralTokenizer();
+            $tokens = $tokenizer->tokenize($allText);
+            
+            $tokenDoc = new TokensDocument($tokens);
+            $tokenDoc->applyTransformation(new LowerCaseFilter())
+            ->applyTransformation(new QuotesFilter())
+            ->applyTransformation(new CharFilter());
 
-    //         $stopWords = new StopWords();
-    //         $tokenizer = new GeneralTokenizer();
-    //         $tokens = array_filter(
-    //             $tokenizer->tokenize($allText),
-    //             fn($word) => !$stopWords->isStopWord($word) && strlen($word) > 3
-    //         );
+            $tokensTransformation = $tokenDoc->getDocumentData();
+            $filteredTokens = array_filter($tokensTransformation, fn($word) => !in_array(strtolower($word), $stopWords) && mb_strlen($word) > 3);    
+            
+            $freqDist = new FreqDist($filteredTokens);
+            dd(array_slice($freqDist->getKeyValuesByFrequency(), 0, 10), true);
+            $top10 = array_splice($freqDist->getKeyValuesByFrequency(), 0, 10);
 
-    //         return response()->json(array_count_values($tokens)); // Ritorna parole più frequenti
-
-    //     } catch (\Exception $e) {
-    //         Log::error("Errore nell'analisi testo: " . $e->getMessage());
-    //         return response()->json(['error' => 'Errore nell\'analisi del testo'], 500);
-    //     }
-    // }
+            return ApiResponse::success($top10, "Testo analizzato con successo", 200);
+        
+        } catch (\Exception $e) {
+            Log::error("Errore nell'analisi testo: {$e->getMessage()} in {$e->getFile()} alla riga {$e->getLine()}");
+            return ApiResponse::error("Errore nell'analisi del testo", 500);
+        }
+    }
 
     // // 📊 3. FILTRO PAROLE CHIAVE CON GOOGLE TRENDS
     // public function filterKeywords()
